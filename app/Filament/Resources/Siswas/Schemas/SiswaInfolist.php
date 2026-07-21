a9f0b3ea0dd4b2880df1f33a2ed22decd1922468
Noval Aly <alynoval10@gmail.com>
2026-07-21 10:09:42 +0700

    commit v2

---
+++ app/Filament/Resources/Siswas/Schemas/SiswaInfolist.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Siswas\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class SiswaInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('nis'),
+                TextEntry::make('nama'),
+                TextEntry::make('kelas'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
