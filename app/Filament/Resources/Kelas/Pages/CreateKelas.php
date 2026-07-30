759040a1f5537c3771eeb7accc18c20d8f5c6ac1
Noval Aly <alynoval10@gmail.com>
2026-07-30 14:46:31 +0700

    Commit V17 Update Simpan Data

--- app/Filament/Resources/Kelas/Pages/CreateKelas.php
+++ app/Filament/Resources/Kelas/Pages/CreateKelas.php
@@ -2,4 +2,10 @@
 namespace App\Filament\Resources\Kelas\Pages;
 use App\Filament\Resources\Kelas\KelasResource;
 use Filament\Resources\Pages\CreateRecord;
-class CreateKelas extends CreateRecord { protected static string $resource = KelasResource::class; }
+class CreateKelas extends CreateRecord { 
+    protected static string $resource = KelasResource::class;
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
+    }
--- app/Filament/Resources/Siswas/Pages/ListSiswas.php
+++ app/Filament/Resources/Siswas/Pages/ListSiswas.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\Siswas\Pages;
 
+use App\Exports\SiswaTemplateExport;
 use App\Filament\Resources\Siswas\SiswaResource;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListSiswas extends ListRecords
 {
@@ -14,6 +17,16 @@ class ListSiswas extends ListRecords
     {
         return [
             CreateAction::make(),
+
+            Action::make('downloadTemplate')
+                ->label('Download Template')
+                ->icon('heroicon-o-document-arrow-down')
+                ->action(function () {
+                    return Excel::download(
+                        new SiswaTemplateExport(),
+                        'template-import-siswa.xlsx'
+                    );
+                }),
         ];
     }
 }
